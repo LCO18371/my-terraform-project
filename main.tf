f0c4847bca079972526f7a52db8c394c47c4ff9c
Ravi <chandravi00123@gmail.com>
2025-03-17 06:40:33 +0000

    main.tf bucket code updated

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ terraform {
   }
 }
 
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-terraform-test" # Replace with a globally unique name
-  acl    = "private"                              # Set to the desired ACL, e.g., "private", "public-read", etc.
-  region = "us-east-1"                            # Specify the region (optional, can be inferred from provider)
+  bucket = "my-unique-bucket-name-terraform-test"
 }
+
